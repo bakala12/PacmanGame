15c7d808deb2fca0f7a4c6f1ca62b9bd4e3b9fd0
Mateusz Bąkała <bakala12@o2.pl>
2016-03-29 13:24:45 +0200

    Adding comments for GameElement

--- GameControls/Elements/GameElement.cs
+++ GameControls/Elements/GameElement.cs
@@ -16,55 +16,107 @@ using GameControls.Interfaces;
 
 namespace GameControls.Elements
 {
-    public class GameElement : Control, IGameElement
+    /// <summary>
+    /// Represents a custom WPF control providing base functionality for all game elements. 
+    /// </summary>
+    public abstract class GameElement : Control, IGameElement
     {
+        /// <summary>
+        /// Static constructor.
+        /// </summary>
         static GameElement()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(GameElement), new FrameworkPropertyMetadata(typeof(GameElement)));
         }
 
+        /// <summary>
+        /// Initializes a new instance of GameElement object and sets all the properties to 0.
+        /// </summary>
+        protected GameElement()
+        {
+            X = 0;
+            Y = 0;
+            ElementHeight = 0;
+            ElementWidth = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of GameElement object with the specified width and height.
+        /// </summary>
+        /// <param name="width">Width of the element.</param>
+        /// <param name="height">Height of the element.</param>
+        protected GameElement(uint width, uint height):this()
+        {
+            ElementHeight = height;
+            ElementWidth = width;
+        }
+
+        /// <summary>
+        /// A DependencyProperty for storing X value.
+        /// </summary>
         public static readonly DependencyProperty XProperty = 
-            DependencyProperty.Register("X", typeof(int), typeof(GameElement),
+            DependencyProperty.Register("X", typeof(uint), typeof(GameElement),
                 new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.AffectsRender|
                     FrameworkPropertyMetadataOptions.AffectsArrange |
                 FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsParentArrange));
 
+        /// <summary>
+        /// A DependencyProperty for storing Y value.
+        /// </summary>
         public static readonly DependencyProperty YProperty = 
-            DependencyProperty.Register("Y", typeof(int), typeof(GameElement), new FrameworkPropertyMetadata(0,
+            DependencyProperty.Register("Y", typeof(uint), typeof(GameElement), new FrameworkPropertyMetadata(0,
                 FrameworkPropertyMetadataOptions.AffectsRender |
                     FrameworkPropertyMetadataOptions.AffectsArrange |
                 FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsParentArrange));
 
+        /// <summary>
+        /// A DependencyProperty for storing ElementWidth value.
+        /// </summary>
         public static readonly DependencyProperty ElementWidthProperty = 
             DependencyProperty.Register("ElementWidth", typeof(uint), typeof(GameElement),
                 new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.AffectsRender));
 
+        /// <summary>
+        /// A DependencyProperty for storing ElementHeight value.
+        /// </summary>
         public static readonly DependencyProperty ElementHeightProperty = 
             DependencyProperty.Register("ElementHeight", typeof(uint), typeof(GameElement), 
                 new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.AffectsRender));
 
+        /// <summary>
+        /// Gets or sets the current X position of the element.
+        /// </summary>
         public uint X
         {
             get { return (uint) GetValue(XProperty); }
             set { SetValue(XProperty, value);}
         }
 
+        /// <summary>
+        /// Gets or sets the current Y position of the element.
+        /// </summary>
         public uint Y
         {
             get { return (uint) GetValue(YProperty); }
             set { SetValue(YProperty, value);}
         }
 
+        /// <summary>
+        /// Gets or sets the width of the element.
+        /// </summary>
         public uint ElementWidth
         {
             get { return (uint) GetValue(ElementWidthProperty); }
-            set { SetValue(ElementWidthProperty, value);}
+            protected set { SetValue(ElementWidthProperty, value);}
         }
 
+        /// <summary>
+        /// Gets or sets the height of the element.
+        /// </summary>
         public uint ElementHeight
         {
             get { return (uint) GetValue(ElementHeightProperty); }
-            set { SetValue(ElementHeightProperty, value);}
+            protected set { SetValue(ElementHeightProperty, value);}
         }
     }
 }
--- GameControls/Interfaces/IGameElement.cs
+++ GameControls/Interfaces/IGameElement.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace GameControls.Interfaces
 {
+    /// <summary>
+    /// Provides the access to basic element's properties.
+    /// </summary>
     public interface IGameElement
     {
+        /// <summary>
+        /// Gets the current X position of the element.
+        /// </summary>
         uint X { get; set; }
+        /// <summary>
+        /// Gets the current Y position of the element.
+        /// </summary>
         uint Y { get; set; }
+        /// <summary>
+        /// Gets the width of the element.
+        /// </summary>
         uint ElementWidth { get; }
+        /// <summary>
+        /// Gets the height of the element.
+        /// </summary>
         uint ElementHeight { get; }
     }
 }
