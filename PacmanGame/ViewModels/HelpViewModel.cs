c0c1c5fdfaac9617622831afc6caa672ded76c05
Mateusz Bąkała <bakala12@o2.pl>
2016-04-09 11:35:38 +0200

    Adding HelpViewModel class

---
+++ PacmanGame/ViewModels/HelpViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PacmanGame.ViewModels
+{
+    public class HelpViewModel : CloseableViewModel
+    {
+        public ICommand ReturnCommand { get; }
+
+        public HelpViewModel() :base("Help")
+        {
+            ReturnCommand = new DelegateCommand(x=>Close());
+        }
+    }
+}
