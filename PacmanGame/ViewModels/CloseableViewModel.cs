17b642227484d2ad794ad0b822710cf424ff1b7c
Mateusz Bąkała <bakala12@o2.pl>
2016-04-09 10:27:24 +0200

    Adding base ViewModels classes

---
+++ PacmanGame/ViewModels/CloseableViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanGame.ViewModels
+{
+    /// <summary>
+    /// Represents a ViewModel that supports close opertation. 
+    /// </summary>
+    public abstract class CloseableViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// An event which is raised when the associated view should be closed.
+        /// </summary>
+        public EventHandler RequestClose;
+
+        /// <summary>
+        /// A method which raises the RequestClose event. 
+        /// </summary>
+        protected virtual void Close()
+        {
+            RequestClose?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ PacmanGame/ViewModels/ViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using PacmanGame.Annotations;
+
+namespace PacmanGame.ViewModels
+{
+    /// <summary>
+    /// Represents a base class for all ViewModels. It supports property changing notification
+    /// by implementing INotifyPropertyChanged interface 
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// An event raised when the property is changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The method which raises the PropertyChanged event for the propertyName property.
+        /// </summary>
+        /// <param name="propertyName">The name of property which has changed.</param>
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
