81a594e2f631463b418e55c52206887c666311a5
Mateusz Bąkała <bakala12@o2.pl>
2016-04-16 18:30:02 +0200

    GameBoard implementation part 1

--- GameControls/Board/GameBoard.xaml.cs
+++ GameControls/Board/GameBoard.xaml.cs
@@ -6,23 +6,58 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GameControls.Elements;
+using Block = GameControls.Elements.Block;
 
 namespace GameControls.Board
 {
     /// <summary>
-    /// Interaction logic for GameBoard.xaml
+    /// Represents a control for the whole game board. 
+    /// It contains all graphical GameElements.
     /// </summary>
     public partial class GameBoard : UserControl
     {
         public GameBoard()
         {
             InitializeComponent();
+            Rows = 1;
+            Columns = 1;
+            Background = Brushes.Orange;
         }
+
+        /// <summary>
+        /// Gets the number of rows of the board.
+        /// </summary>
+        public uint Rows
+        {
+            get { return (uint)GetValue(RowsProperty); }
+            private set { SetValue(RowsProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets the number of columns of the board.
+        /// </summary>
+        public uint Columns
+        {
+            get { return (uint)GetValue(ColumnsProperty); }
+            private set { SetValue(ColumnsProperty, value); }
+        }
+
+        /// <summary>
+        /// A DependencyProperty for storing Rows property.
+        /// </summary>
+        public static readonly DependencyProperty RowsProperty = 
+            DependencyProperty.Register("Rows", typeof(uint), typeof(GameBoard), new FrameworkPropertyMetadata(0));
+
+        /// <summary>
+        /// A DependencyProperty for storing Columns property.
+        /// </summary>
+        public static readonly DependencyProperty ColumnsProperty = 
+            DependencyProperty.Register("Columns", typeof(uint), typeof(GameBoard), new FrameworkPropertyMetadata(0));
     }
 }
