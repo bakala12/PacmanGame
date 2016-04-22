0b4849c6bd3b6a6d045d4baefc2fa62a38b3369d
Mateusz Bąkała <bakala12@o2.pl>
2016-04-22 22:43:05 +0200

    Input bindings and MoveCommand

--- PacmanGame/ViewModels/GameViewModel.cs
+++ PacmanGame/ViewModels/GameViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using GameControls.Board;
 using PacmanGame.BoardGenerator;
 
@@ -18,6 +20,7 @@ namespace PacmanGame.ViewModels
             var s = Application.GetResourceStream(new Uri("Resources/example_board.board", UriKind.Relative));
             IGameBoardGenerator generator = new ExampleFileGameBoardGenerator(s.Stream);
             GameBoard = generator.GenerateBoard(30, 30, 1);
+            MoveCommand = new DelegateCommand(MovePlayer);
         }
 
         private GameBoard _gameBoard;
@@ -30,6 +33,15 @@ namespace PacmanGame.ViewModels
                 _gameBoard = value;
                 OnPropertyChanged();
             }
+        }
+
+        public ICommand MoveCommand { get; }
+
+        private void MovePlayer(object parameter)
+        {
+            Key key = parameter as Key? ?? Key.None;
+            if(key==Key.None) return;
+            //here move in engine
         }
     }
 }
