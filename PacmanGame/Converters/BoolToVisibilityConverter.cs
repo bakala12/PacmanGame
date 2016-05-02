317ede98c9c07075bfdd5981f801a97903d663ed
Mateusz Bąkała <bakala12@o2.pl>
2016-05-02 17:08:08 +0200

    Add BoolToVisibilityConverter class

---
+++ PacmanGame/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PacmanGame.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool v = value as bool? ?? false;
+            return v ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
