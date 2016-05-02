5a758d63392320c3f7a0df1fee633a1e23be6289
Mateusz Bąkała <bakala12@o2.pl>
2016-05-02 19:47:02 +0200

    Fixing sorting highscores

--- PacmanGame/Highscores/HighscoreComparer.cs
+++ PacmanGame/Highscores/HighscoreComparer.cs
@@ -11,8 +11,8 @@ namespace PacmanGame.Highscores
         public int Compare(Highscore x, Highscore y)
         {
             int r= (int)x.Points - (int)y.Points;
-            if (r != 0) return r;
-            return x.GameTime < y.GameTime ? -1 : 1;
+            if (r != 0) return -r;
+            return x.GameTime < y.GameTime ? 1 : -1;
         }
     }
 }
